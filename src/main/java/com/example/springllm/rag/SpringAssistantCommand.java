c6949a03ca9026b1c69a7ae305b58ec9ab7cdba2
Sepideh Vaziry <sepideh.vaziry@gmail.com>
2025-04-29 16:29:42 +0330

    Chore: Little changes

--- src/main/java/com/example/springllm/rag/SpringAssistantCommand.java
+++ src/main/java/com/example/springllm/rag/SpringAssistantCommand.java
@@ -1,10 +1,18 @@
 package com.example.springllm.rag;
 
-import groovyjarjarpicocli.CommandLine.Command;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor;
+import org.springframework.ai.chat.prompt.PromptTemplate;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.core.io.Resource;
+import org.springframework.shell.command.annotation.Command;
 
 @Command
 public class SpringAssistantCommand {
@@ -15,27 +23,36 @@ public class SpringAssistantCommand {
   @Value("classpath:/prompts/spring-boot-reference.st")
   private Resource sbPromptTemplate;
 
-  public SpringAssistantCommand(ChatClient chatClient, VectorStore vectorStore) {
-    this.chatClient = chatClient;
+  public SpringAssistantCommand(ChatClient.Builder builder, VectorStore vectorStore) {
     this.vectorStore = vectorStore;
+
+    this.chatClient = builder
+        .defaultAdvisors(new QuestionAnswerAdvisor(vectorStore,SearchRequest.builder().topK(2).build()))
+        .build();
   }
 
-//  @Command(command = "q")
-//  public String question(@DefaultValue(value = "What is Spring Boot") String message) {
-//    PromptTemplate promptTemplate = new PromptTemplate(sbPromptTemplate);
-//    Map<String, Object> promptParameters = new HashMap<>();
-//    promptParameters.put("input", message);
-//    promptParameters.put("documents", String.join("\n", findSimilarDocuments(message)));
-//
-//    return chatClient.call(promptTemplate.create(promptParameters))
-//        .getResult()
-//        .getOutput()
-//        .getContent();
-//  }
-//
-//  private List<String> findSimilarDocuments(String message) {
-//    List<Document> similarDocuments = vectorStore.similaritySearch(SearchRequest.query(message).withTopK(3));
-//    return similarDocuments.stream().map(Document::getContent).toList();
-//  }
+  @Command(command = "q")
+  public String question(@DefaultValue(value = "What is Spring Boot") String message) {
+    PromptTemplate promptTemplate = new PromptTemplate(sbPromptTemplate);
+
+    Map<String, Object> promptParameters = new HashMap<>();
+    promptParameters.put("input", message);
+    promptParameters.put("documents", String.join("\n", findSimilarDocuments(message)));
+
+    return chatClient.prompt(promptTemplate.create(promptParameters))
+        .call()
+        .content();
+  }
+
+  private List<String> findSimilarDocuments(String message) {
+    List<Document> similarDocuments = vectorStore.similaritySearch(
+        new SearchRequest.Builder()
+            .query(message)
+            .topK(3)
+            .build()
+    );
+
+    return similarDocuments.stream().map(Document::getFormattedContent).toList();
+  }
 
 }
