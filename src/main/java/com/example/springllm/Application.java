5df8ef64643e3d45c1e7fdc49714e8492cb28bb1
Sepideh Vaziry <sepideh.vaziry@gmail.com>
2025-04-30 16:09:58 +0330

    Chore: Little changes

--- src/main/java/com/example/springllm/Application.java
+++ src/main/java/com/example/springllm/Application.java
@@ -1,11 +1,14 @@
 package com.example.springllm;
 
+import com.example.springllm.rag.HintsRegistrar;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportRuntimeHints;
 import org.springframework.shell.command.annotation.CommandScan;
 
 @CommandScan
 @SpringBootApplication
+@ImportRuntimeHints(HintsRegistrar.class)
 public class Application {
 
 	public static void main(String[] args) {
