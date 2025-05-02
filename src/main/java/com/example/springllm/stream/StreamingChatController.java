5cd8560bec5f98507940e130a77a5606797cfc68
Sepideh Vaziry <sepideh.vaziry@gmail.com>
2025-05-02 17:10:30 +0330

    Feature: Provide a single html page to chat with bot

---
+++ src/main/java/com/example/springllm/stream/StreamingChatController.java
@@ -0,0 +1,37 @@
+package com.example.springllm.stream;
+
+import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Flux;
+
+@CrossOrigin
+@RestController
+public class StreamingChatController {
+
+  private final ChatClient chatClient;
+
+  public StreamingChatController(ChatClient.Builder builder) {
+    this.chatClient = builder.build();
+  }
+
+  @PostMapping("/chat")
+  public String chat(@RequestParam String message) {
+    return chatClient.prompt()
+        .user(message)
+        .call()
+        .content();
+  }
+
+  @GetMapping("/stream")
+  public Flux<String> chatWithStream(@RequestParam String message) {
+    return chatClient.prompt()
+        .user(message)
+        .stream()
+        .content();
+  }
+
+}
